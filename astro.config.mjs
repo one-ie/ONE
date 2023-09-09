cffb6f65532385d58635d29b532b50d32eec306e
Anthony OConnell <tony@one.ie>
2023-09-09 14:22:10 +0700

    Test

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,12 +4,10 @@ import preact from "@astrojs/preact";
 import tailwind from '@astrojs/tailwind';
 import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
-import cloudflare from "@astrojs/cloudflare";
-import aws from "astro-sst/edge";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: aws(),
+  adapter: vercel(),
   integrations: [tailwind(), svelte(), preact(), react()]
 });
