ea960552f952d94b724acaf54ee3b2818c54d1a9
Anthony OConnell <tony@one.ie>
2023-11-20 10:22:56 +0700

    Remove Vercel and Netlify

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from 'astro/config';
 import svelte from '@astrojs/svelte';
 import preact from "@astrojs/preact";
 import tailwind from '@astrojs/tailwind';
-import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
 
 import cloudflare from "@astrojs/cloudflare";
