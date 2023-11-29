2073839e998db7acfc7724e37c7cdda014dccbfa
Anthony OConnell <tony@one.ie>
2023-11-29 18:00:56 +0700

    Clean Up

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,27 @@
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
-    extend: {},
+    extend: {
+      // Customizing font sizes
+      fontSize: {
+        xs: '.75rem', // 12px
+        sm: '.875rem', // 14px
+        base: '1rem', // 16px
+        lg: '1.125rem', // 18px
+        xl: '1.25rem', // 20px
+        '2xl': '1.5rem', // 24px
+        '3xl': '1.875rem', // 30px
+        '4xl': '2.25rem', // 36px
+        '5xl': '3rem', // 48px
+        '6xl': '4rem', // 64px
+        '7xl': '5rem', // 80px
+        // Add more sizes as needed
+      },
+    },
   },
-  plugins: [require('daisyui'), '@tailwindcss/typography'],
-  // daisyUI config (optional - here are the default values)
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
+  
+ // daisyUI config
   daisyui: {
     themes: [
       {
