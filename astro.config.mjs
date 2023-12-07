862a2df0b141af1c5d94ff672cb93fae14cda935
Anthony OConnell <tony@one.ie>
2023-12-08 00:12:59 +0700

    Upgrade

--- astro.config.mjs
+++ astro.config.mjs
@@ -14,7 +14,7 @@ export default defineConfig({
     adapter: cloudflare({
     mode: 'server',
     routes: {
-      strategy: 'include',
+      strategy: 'exclude',
       include: ['/*'], // handled by custom function: functions/users/[id].js
       exclude: ['/'], // handled by static page: pages/users/faq.astro
     },
