68d30acff706ffad116196e6d486dd9389cc6b2a
Anthony OConnell <tony@one.ie>
2023-12-08 03:14:02 +0700

    Partytown

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,12 +10,20 @@ import cloudflare from "@astrojs/cloudflare";
 
 import critters from "astro-critters";
 import serviceWorker from "astrojs-service-worker";
-
 import prefetch from "@astrojs/prefetch";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare({}),
-  integrations: [tailwind(), svelte(), preact(), react(), mdx(), prefetch(), serviceWorker(), critters()]
+  integrations: [tailwind(), svelte(), preact(), react(), mdx(), prefetch({
+    // Allow up to three links to be prefetched concurrently
+    throttle: 3
+  }), serviceWorker(),  partytown({
+      // Example: Add dataLayer.push as a forwarding-event.
+      config: {
+        forward: ['dataLayer.push', 'fbq']
+      },
+    }), critters()]
 });
