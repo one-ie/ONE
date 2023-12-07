f2a9a17883e3bc0ac48d4e32b24ea5f6dde72a0a
Anthony OConnell <tony@one.ie>
2023-12-07 22:04:54 +0700

    Theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -28,19 +28,39 @@ module.exports = {
     themes: [
       {
         ONE: {
-          primary: 'black', // Primary Color
-          secondary: 'yellow', // Secondary Color
-          accent: 'blue', // Accent Color
-          neutral: 'green', // Neutral Color
+          neutral: '#111111',
+          primary: '#1d3963',
+          secondary: '#394e32',
+          'border-primary': '#ffffff',
+          'focus-secondary': '#23321e',
+          'border-secondary': '#23321e',
+          'focus-primary': '#102646',
+          'content-primary': '#d9d9d9',
+          'content-secondary': '#ffffff',
+          accent: '#ffffff',
+          'border-accent': '#4b5563',
+          'focus-neutral': '#111111',
+          'border-neutral': '#ffffff',
+          'content-neutral': '#ffffff',
           // Base Colors for Background
-          'base-100': '#333', // Main Color
-          'base-200': 'purple', // Slightly Darker or Lighter
+          'base-100': '#111', // Main Color
+          'base-200': '#222', // Slightly Darker or Lighter
           'base-300': '#333', // Even more dark or light
           // Info Colors
           info: '#77a2f8',
           success: '#21dec5',
           warning: '#c17915',
           error: '#f22618',
+          // Styles
+          '--rounded-box': '1rem', // border radius rounded-box utility class, used in card and other large boxes
+          '--rounded-btn': '3.5rem', // border radius rounded-btn utility class, used in buttons and similar element
+          '--rounded-badge': '1.9rem', // border radius rounded-badge utility class, used in badges and similar
+          '--animation-btn': '0.25s', // duration of animation when you click on button
+          '--animation-input': '0.2s', // duration of animation for inputs like checkbox, toggle, radio, etc
+          '--btn-focus-scale': '0.95', // scale transform of button when you focus on it
+          '--border-btn': '1px', // border width of buttons
+          '--tab-border': '1px', // border width of tabs
+          '--tab-radius': '0.5rem', // border radius of tabs
         },
         TWO: {
           primary: '#1D3963',
