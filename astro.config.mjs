7f7a4ee80673f296ed8d1d9f0c52386cd9643ef8
Anthony OConnell <tony@one.ie>
2023-12-08 01:57:32 +0700

    Service Worker

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,10 +9,11 @@ import cloudflare from "@astrojs/cloudflare";
 // Change your adaptor
 
 import critters from "astro-critters";
+import serviceWorker from "astrojs-service-worker";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: cloudflare({}),
-  integrations: [tailwind(), svelte(), preact(), react(), mdx(), critters()]
+  integrations: [tailwind(), svelte(), preact(), react(), mdx(), serviceWorker(), critters()]
 });
