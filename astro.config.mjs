72e0c249c2e23c98b0bede8771a8533908ccab4b
Anthony OConnell <tony@one.ie>
2023-12-07 23:33:32 +0700

    Upgrade

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,15 +6,13 @@ import preact from "@astrojs/preact";
 import react from "@astrojs/react";
 import cloudflare from "@astrojs/cloudflare";
 
-// Change your adaptor
-
 // https://astro.build/config
 export default defineConfig({
   output: "server",
     adapter: cloudflare({
     mode: 'server',
     routes: {
-      strategy: 'auto',
+      strategy: 'exclude',
     },
   }),
   integrations: [tailwind(), svelte(), preact(), react(), mdx()]
