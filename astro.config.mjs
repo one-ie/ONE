95801805ab99c3e03be6953c1263c25f8c4b0aad
Anthony OConnell <tony@one.ie>
2023-12-08 01:20:28 +0700

    Critters

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,9 +8,11 @@ import cloudflare from "@astrojs/cloudflare";
 
 // Change your adaptor
 
+import critters from "astro-critters";
+
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-    adapter: cloudflare({}),
-  integrations: [tailwind(), svelte(), preact(), react(), mdx()]
+  adapter: cloudflare({}),
+  integrations: [tailwind(), svelte(), preact(), react(), mdx(), critters()]
 });
