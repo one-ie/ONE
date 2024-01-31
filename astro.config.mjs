f68c3c9b7f78db3695e6f6ac12bc1337f871af4e
Anthony O'Connell <tony@one.ie>
2024-02-01 02:43:43 +0700

    Remove Astro Robots

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
-import robots from "astro-robots";
 
 // https://astro.build/config
 export default defineConfig({
@@ -22,7 +21,6 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
-    robots(),
   ],
   adapter: cloudflare(),
   output: "hybrid",
