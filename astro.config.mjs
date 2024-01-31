a186d897a9eed7142e6d9621a814f2f5719c9308
Anthony O'Connell <tony@one.ie>
2024-02-01 00:46:21 +0700

    Add Robots.txt Generator

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
-
 import robots from "astro-robots";
 
 // https://astro.build/config
