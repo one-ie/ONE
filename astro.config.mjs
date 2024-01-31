98d3ba6621d298f3bc22009e6106fa23141d4e51
Anthony O'Connell <tony@one.ie>
2024-02-01 02:56:08 +0700

    Critters

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,23 +5,20 @@ import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 
+import critters from "astro-critters";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://new.one.ie",
-  integrations: [
-    mdx({
-      syntaxHighlight: "shiki",
-      shikiConfig: {
-        theme: "github-dark-dimmed",
-      },
-      gfm: true,
-    }),
-    sitemap(),
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [mdx({
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: "github-dark-dimmed"
+    },
+    gfm: true
+  }), sitemap(), react(), tailwind({
+    applyBaseStyles: false
+  }), critters()],
   adapter: cloudflare(),
-  output: "hybrid",
+  output: "hybrid"
 });
