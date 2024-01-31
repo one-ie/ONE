c3be831001d02e9379592c69f264ed671da2d140
Anthony O'Connell <tony@one.ie>
2024-02-01 00:35:46 +0700

    Add Robots.txt Generator

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,9 @@ import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
-import vercel from '@astrojs/vercel/static';
-
 import cloudflare from "@astrojs/cloudflare";
+
+import robots from "astro-robots";
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,7 +18,7 @@ export default defineConfig({
     gfm: true
   }), sitemap(), react(), tailwind({
     applyBaseStyles: false
-  })],
+  }), robots()],
   adapter: cloudflare(),
   output: "hybrid"
 });
