5c80f33e81eb6fbbce3b754f03e0a6180a7926f6
Anthony O'Connell <tony@one.ie>
2024-02-25 22:28:32 +0700

    Astro Icon Upgraded

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,22 +3,27 @@ import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
+import icon from "astro-icon";
 import cloudflare from "@astrojs/cloudflare";
-
 import compressor from "astro-compressor";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://new.one.ie",
-  integrations: [mdx({
-    syntaxHighlight: "shiki",
-    shikiConfig: {
-      theme: "github-dark-dimmed"
-    },
-    gfm: true
-  }), sitemap(), react(), tailwind({
-    applyBaseStyles: false
-  }), compressor()],
+  integrations: [
+    mdx({
+      syntaxHighlight: "shiki",
+      shikiConfig: { theme: "github-dark-dimmed" },
+      gfm: true,
+    }),
+    icon(),
+    sitemap(),
+    react(),
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    compressor(),
+  ],
   adapter: cloudflare(),
-  output: "hybrid"
+  output: "hybrid",
 });
