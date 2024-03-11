cbed2e0a3b4f10b8c67aaac6e788a90ea4731221
Anthony O'Connell <tony@one.ie>
2024-03-11 18:32:23 +0700

    blocks

---
+++ src/blocks/se.svelte
@@ -0,0 +1,16 @@
+<script>
+  import { loadStripe } from '@stripe/stripe-js'
+  import { Elements } from 'svelte-stripe'
+  import { onMount } from 'svelte'
+  import { PUBLIC_STRIPE_KEY } from '$env/static/public'
+
+  let stripe = null
+
+  onMount(async () => {
+    stripe = await loadStripe(PUBLIC_STRIPE_KEY)
+  })
+</script>
+
+<Elements {stripe}>
+  <!-- this is where your Stripe components go -->
+</Elements>
