0835765dd2b6fad4dd22e46fc5b4e98f1bdd9317
Anthony O'Connell <tony@one.ie>
2024-04-29 03:31:03 +0700

    supabase

--- astro.config copy.mjs
+++ astro.config copy.mjs
@@ -6,22 +6,22 @@ import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import cloudflare from "@astrojs/cloudflare";
 
+import netlify from "@astrojs/netlify";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://one.ie",
-  integrations: [
-    mdx({
-      syntaxHighlight: "shiki",
-      shikiConfig: { theme: "github-dark-dimmed" },
-      gfm: true,
-    }),
-    icon(),
-    sitemap(),
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    })
-  ],
-adapter: cloudflare({
-  }),  output: "server",
+  integrations: [mdx({
+    syntaxHighlight: "shiki",
+    shikiConfig: {
+      theme: "github-dark-dimmed"
+    },
+    gfm: true
+  }), icon(), sitemap(), react(), tailwind({
+    applyBaseStyles: false
+  })],
+output: 'server',
+  adapter: netlify({
+    edgeMiddleware: true
+  }),
 });
