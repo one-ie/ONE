206fa979ec5f7f6febbaabd382169f0e71d303a8
Anthony O'Connell <tony@one.ie>
2024-04-30 11:21:34 +0700

    All Working

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,8 @@
+import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import react from "@astrojs/react";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import { defineConfig } from "astro/config";
 import icon from "astro-icon";
 import cloudflare from "@astrojs/cloudflare";
 
